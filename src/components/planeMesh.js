4843ce702f8e53d505e96679d361e31fe181e369
Filippe Braga <filippeffx@hotmail.com>
2020-11-24 22:20:05 -0300

    Added the model

--- src/components/planeMesh.js
+++ src/components/planeMesh.js
@@ -4,6 +4,7 @@ const Plane = () => (
     rotation={[-Math.PI / 2, 0, 0]} 
     position={[0, -3, 0]} >
     <planeBufferGeometry attach='geometry' args={[100, 100]} />
+    {/* <meshPhysicalMaterial attach='material' color='red' /> */}
     <shadowMaterial attach='material' opacity={.3} />
     {/** This is necessary to cast a shadow */}
   </mesh>
