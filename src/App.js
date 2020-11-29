f9cb379b4ad6dfefa945798e73ad0fa055f99e46
Filippe Braga <filippeffx@hotmail.com>
2020-11-28 23:07:01 -0300

    model working in the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Canvas } from 'react-three-fiber';
 import { softShadows } from 'drei';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import SpinningMesh from './components/spinningMesh';
 import PlaneMesh from './components/planeMesh';
@@ -11,6 +13,18 @@ import Lights from './components/Lights';
 import './App.scss';
 
 softShadows();
+
+const SkullMesh = () => {
+  const [model, setModel] = useState();
+
+  useEffect(() => {
+    new GLTFLoader().load("/scene.glb", setModel);
+  }, []);
+
+  // console.log(model);
+
+  return model ? <primitive object={model.scene} /> : null;
+}
 
 function App() {  
   return (
@@ -26,6 +40,8 @@ function App() {
         <Controls />   
         
         <PlaneMesh />
+
+        <SkullMesh />
 
         <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue' />
         <SpinningMesh position={[-2, 1, -5]} color='pink' />
